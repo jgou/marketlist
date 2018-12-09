53bd20056dc6b57ca917e6707c175a3f7e376d71
jgou <jordi.gou@gmail.com>
2018-12-09 23:11:42 +0100

    User list accessible by Administrators

---
+++ Controllers/UserManagementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketList.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketList.Controllers
+{
+    [Authorize (Roles = "Administrator")]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserManagementController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admins = (await _userManager.GetUsersInRoleAsync("Administrator")).ToArray();
+            var everyone = await _userManager.Users.ToArrayAsync();
+
+            var model = new UserManagementViewModel
+            {
+                Administrators = admins,
+                Everyone = everyone
+            };
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Models/UserManagementViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MarketList.Models
+{
+    public class UserManagementViewModel
+    {
+        public IdentityUser[] Administrators { get; set; }
+        public IdentityUser[] Everyone { get; set; }
+    }
+}
